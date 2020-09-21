b327dd836d082c1641a16c140c93e1db7349dd20
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-09-22 00:05:49 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/investor-oceanenergy/';
-// $config['base_url'] = 'https://investor-oceanenergy.com/';
+// $config['base_url'] = 'http://localhost/investor-oceanenergy/';
+$config['base_url'] = 'https://investor-oceanenergy.com/';
 
 /*
 |--------------------------------------------------------------------------
